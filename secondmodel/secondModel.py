3dd63ed9a8892bbb8a5ad5cd55c48f3887743497
Shingo <eclair.1008@gmail.com>
2017-01-29 23:06:58 +0900

    add secondmodel

---
+++ secondmodel/secondModel.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from MoneyModel import *
+
+model = MoneyModel(50, 10, 10)
+for i in range(20):
+    model.step()
+
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for cell in model.grid.coord_iter():
+    cell_content, x, y = cell
+    agent_count = len(cell_content)
+    agent_counts[x][y] = agent_count
+plt.imshow(agent_counts, interpolation='nearest')
+plt.colorbar()
+plt.show()
